ded65226267d8e375d2d33e33df4a266b6c244b7
Nicholas Vanderlinden <watchout8181@gmail.com>
2022-07-28 09:05:59 +0200

    initial

---
+++ src/main/java/applications/tricentis/pages/HomePage.java
@@ -0,0 +1,9 @@
+package applications.tricentis.pages;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+    private final By loginButton = By.xpath("//a[text()=\"Log in\"]");
+    private final By cartButton = By.xpath("//span[text()=\"Shopping cart\"]");
+
+}
