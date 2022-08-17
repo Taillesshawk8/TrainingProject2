7139f4c367a8a3b2ad4a6120b6a8df5ab32d0131
Nicholas Vanderlinden <watchout8181@gmail.com>
2022-08-17 11:40:20 +0200

    added tricentis page object model

---
+++ src/main/java/applications/saucedemo/pages/NicholasVanderlinden.java
@@ -0,0 +1,12 @@
+package applications.saucedemo.pages;
+
+public class NicholasVanderlinden {
+
+    public String username;
+    public String password;
+
+    public NicholasVanderlinden(String user, String pass) {
+        this.username = user;
+        this.password = pass;
+    }
+}
